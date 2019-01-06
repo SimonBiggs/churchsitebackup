f415a539c489ad508cf84a0778921adc2a44298a
Simon Biggs <me@simonbiggs.net>
2019-01-06 16:23:55 +1100

    init

---
+++ www.waggachurchofchrist.org.au/wp-login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-AU">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-AU">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; Wagga Church of Christ &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='https://s.w.org/' />
+<link rel='stylesheet' href='wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load[]=dashicons,buttons,forms,l10n,login&amp;ver=4.9.9' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="wp-content/uploads/2016/07/cropped-cross_sml_2-32x32.png" sizes="32x32" />
+<link rel="icon" href="wp-content/uploads/2016/07/cropped-cross_sml_2-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="wp-content/uploads/2016/07/cropped-cross_sml_2-180x180.png" />
+<meta name="msapplication-TileImage" content="https://www.waggachurchofchrist.org.au/wp-content/uploads/2016/07/cropped-cross_sml_2-270x270.png" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-en-au">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="https://www.waggachurchofchrist.org.au/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="index.html">&larr; Back to Wagga Church of Christ</a></p>
+		
+	</div>
+
+	
+	<script type='text/javascript' src='wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script>jQuery(document).ready(function(){;jQuery('#login h1 a').attr('title','Wagga Church of Christ');jQuery('#login h1 a').attr('href','https://www.waggachurchofchrist.org.au');});</script><style type="text/css">body.login{background-color:#FFF!important;}#login h1 a, .login h1 a { background-image: url(wp-content/uploads/2016/07/logo-300x40.png);width:auto!important;max-width:100%;background-size:contain;background-position-y: center;}@media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) { 
+                #login h1 a, .login h1 a { background-image: url(wp-content/uploads/2016/07/logo-300x40.png);}
+            }</style>	<div class="clear"></div>
+	</body>
+	</html>
+	
